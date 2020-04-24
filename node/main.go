46459c0e4d47c0de99dcf4d5956b3b8bd090d285
Lokman Rahmani <lokman.rahmani@fetch.ai>
2020-04-24 19:15:18 +0100

    typo

--- node/main.go
+++ node/main.go
@@ -8,7 +8,7 @@ import (
 	"errors"
 	"github.com/perlin-network/noise"
 	"github.com/perlin-network/noise/kademlia"
-  "github.com/lrahmani/aea_nose_node/api"	
+  "github.com/lrahmani/aea_nose_node/aea"	
   "time"
 )
 
